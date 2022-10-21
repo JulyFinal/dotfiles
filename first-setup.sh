ef0f52d8dcf5933ff57ec3356d3455cc4628935a
julyfinal <julyfinal@icloud.com>
2022-10-21 10:21:15 +0800

    u sh

--- first-setup.sh
+++ first-setup.sh
@@ -3,9 +3,12 @@
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 
 ## install base pkg
-sudo pacman -S fd ripgrep xsel fzf bat bottom fd lsd sd tealdeer zoxide
+sudo pacman -S fd ripgrep xsel fzf bat bottom fd lsd sd tealdeer zoxide zip unzip
 
 sudo pacman -S neovim lazygit proxychains-ng
-yay -S clash-for-windows-bin
 
-yay -S wps-office wps-office-mui-zh-cn ttf-wps-fonts
+sudo pacman -S zip unzip
+
+# yay -S clash-for-windows-bin
+
+# yay -S wps-office wps-office-mui-zh-cn ttf-wps-fonts
