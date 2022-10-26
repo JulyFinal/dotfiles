7ee3c4d969b514636548c108abf164989782dee8
julyfinal <julyfinal@icloud.com>
2022-10-26 16:39:02 +0800

    u nvim

--- nvim/lua/pconfs/bufferline.lua
+++ nvim/lua/pconfs/bufferline.lua
@@ -1,3 +1,13 @@
 local bufferline = require("bufferline")
 
-bufferline.setup({})
+bufferline.setup({
+  options = {
+    diagnostics = "nvim_lsp";
+    offsets = { {
+      filetype = "NvimTree",
+      text = "File Explorer",
+      highlight = "Directory",
+      text_align = "left"
+    } }
+  }
+})
