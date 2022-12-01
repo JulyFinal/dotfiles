7d818d264b9eca37912e6eb33dbcc307b34d5201
julyfinal <julyfinal@icloud.com>
2022-12-01 10:21:14 +0800

    u nvim

--- nvim/lua/pconfs/toggleterm.lua
+++ nvim/lua/pconfs/toggleterm.lua
@@ -4,3 +4,20 @@ toggleterm.setup({
   open_mapping = [[<c-\>]],
   direction = 'float',
 })
+
+local Terminal = require('toggleterm.terminal').Terminal
+local lazygit  = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  hidden = true,
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
