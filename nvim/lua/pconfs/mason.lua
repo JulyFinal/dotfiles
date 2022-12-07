299d3726a252f58183fe384885acc345f17865a6
julyfinal <julyfinal@icloud.com>
2022-12-07 16:39:43 +0800

    u nvim

--- nvim/lua/pconfs/mason.lua
+++ nvim/lua/pconfs/mason.lua
@@ -126,4 +126,4 @@ vim.diagnostic.config({
   },
 })
 -- when save , format code
-vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
