000746eb4d2a58eb016d291e4fde44a277c00668
julyfinal <julyfinal@icloud.com>
2022-12-09 17:42:13 +0800

    u nvim

--- nvim/lua/pconfs/telescope.lua
+++ nvim/lua/pconfs/telescope.lua
@@ -1,8 +1,8 @@
-local builtin = require('telescope.builtin')
+local builtin = require("telescope.builtin")
 
 local map = vim.keymap.set -- 复用 opt 参数
 
-map('n', 'ff', builtin.find_files, {})
-map('n', 'fg', builtin.live_grep, {})
-map('n', 'fb', builtin.buffers, {})
-map('n', 'fh', builtin.help_tags, {})
+map("n", "<leader>ff", builtin.find_files, {})
+map("n", "<leader>fg", builtin.live_grep, {})
+map("n", "<leader>fb", builtin.buffers, {})
+map("n", "<leader>fh", builtin.help_tags, {})
