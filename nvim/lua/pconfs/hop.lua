ac715acffb00a9c016b3cde0a52b53b40fa7370f
julyfinal <julyfinal@icloud.com>
2023-01-09 18:06:01 +0800

    u nvim hop

--- nvim/lua/pconfs/hop.lua
+++ nvim/lua/pconfs/hop.lua
@@ -1,22 +1,22 @@
-local hop = require('hop')
+local hop = require("hop")
 
-hop.setup { keys = 'etovxqpdygfblzhckisuran' }
+hop.setup({ keys = "etovxqpdygfblzhckisuran" })
 
-local directions = require('hop.hint').HintDirection
+local directions = require("hop.hint").HintDirection
 
-vim.keymap.set('', 'f', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+vim.keymap.set("", "f", function()
+	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
 end, { remap = true })
-vim.keymap.set('', 'F', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+vim.keymap.set("", "F", function()
+	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
 end, { remap = true })
-vim.keymap.set('', 't', function()
-  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+vim.keymap.set("", "t", function()
+	hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
 end, { remap = true })
-vim.keymap.set('', 'T', function()
-  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+vim.keymap.set("", "T", function()
+	hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
 end, { remap = true })
 
-vim.keymap.set('n', ';', function()
-  hop.hint_char1({})
+vim.keymap.set("n", ";", function()
+	hop.hint_char2({})
 end, { remap = true })
