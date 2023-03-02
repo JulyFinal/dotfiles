5b9157b98b2c32dafeb4f4b6fa1e826de627f58c
julyfinal <julyfinal@icloud.com>
2023-03-02 12:00:00 +0800

    u

---
+++ arch-init.sh
@@ -0,0 +1,17 @@
+
+git clone git@github.com:zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+git clone git@github.com:zdharma-continuum/fast-syntax-highlighting $HOME/.zsh/fsh
+git clone git@github.com:zsh-users/zsh-completions $HOME/.zsh/zsh-completions
+
+cargo install starship
+
+## install base pkg
+sudo pacman -S fd ripgrep xsel fzf bat bottom fd lsd sd tealdeer zoxide zip unzip direnv
+sudo pacman -S neovim lazygit proxychains-ng
+sudo pacman -S zip unzip
+
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git && cd paru &&makepkg -si
+
+paru -S clash-for-windows-bin
+# paru -S wps-office wps-office-mui-zh-cn ttf-wps-fonts
--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -3,22 +3,14 @@
 dotfiles="$HOME/dotfiles"
 config="$HOME/.config"
 
-# echo ""
-# if [ -d "$dotfiles" ]; then
-#     echo "Symlinking dotfiles from $dotfiles"
-# else
-#     echo "$dotfiles does not exist"
-#     exit 1
-# fi
-#
-# echo ""
-# if [ -d "$config" ]; then
-#     echo "Symlinking dotfiles to $config"
-# else
-#     echo "$config does not exist"
-#     exit 1
-# fi
-#
+echo ""
+if [ -d "$config" ]; then
+    echo "Symlinking dotfiles to $config"
+else
+    echo "$config does not exist"
+    exit 1
+fi
+
 link() {
     from="$1"
     to="$2"
@@ -27,12 +19,6 @@ link() {
     ln -s "$from" "$to"
 }
 
-# link "$dotconfig/nvim" "$config/nvim"
-
-
-# for name in `ls home`; do
-#     link "$dotfiles/home/$name" "$HOME/$name"
-# done
 link "$dotfiles/home/zshrc" "$HOME/.zshrc"
 link "$dotfiles/home/ideavimrc" "$HOME/.ideavimrc"
 
