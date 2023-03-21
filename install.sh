5a740fee37c23f69dd193c47b92582c097054461
julyfinal <julyfinal@icloud.com>
2023-03-21 11:32:05 +0800

    u

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 [ "${SHELL##/*/}" != "zsh" ] && echo 'You might need to change default shell to zsh: `chsh -s /bin/zsh`'
 
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+
 cargo install starship
 
 git clone https://github.com/JulyFinal/dotfiles.git
@@ -11,8 +13,8 @@ git clone git@github.com:zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosugges
 git clone git@github.com:zdharma-continuum/fast-syntax-highlighting $HOME/.zsh/fsh
 git clone git@github.com:zsh-users/zsh-completions $HOME/.zsh/zsh-completions
 # git clone https://ghproxy.com/https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.zsh/zsh-autosuggestions
-# git clone https://ghproxy.com/https://github.com/fast-syntax-highlighting $HOME/.zsh/fsh
-# git clone https://ghproxy.com/https://github.com/zsh-completions $HOME/.zsh/zsh-completions
+# git clone https://ghproxy.com/https://github.com/zdharma-continuum/fast-syntax-highlighting.git $HOME/.zsh/fsh
+# git clone https://ghproxy.com/https://github.com/zsh-users/zsh-completions.git $HOME/.zsh/zsh-completions
 
 cargo install starship
 
---
+++ readme.md
@@ -0,0 +1,4 @@
+
+# final's dotfiles
+
+## Nothing
