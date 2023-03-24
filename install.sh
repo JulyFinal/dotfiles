c04cddb324cb11bbd3e5acc713cd98e9346a8689
julyfinal <julyfinal@icloud.com>
2023-03-24 09:31:51 +0800

    u

--- install.sh
+++ install.sh
@@ -9,14 +9,6 @@ cargo install starship
 git clone https://github.com/JulyFinal/dotfiles.git
 # git clone git@github.com:JulyFinal/dotfiles.git
 
-# git clone git@github.com:zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
-# git clone git@github.com:zdharma-continuum/fast-syntax-highlighting $HOME/.zsh/fsh
-# git clone git@github.com:zsh-users/zsh-completions $HOME/.zsh/zsh-completions
-
-# git clone https://ghproxy.com/https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.zsh/zsh-autosuggestions
-# git clone https://ghproxy.com/https://github.com/zdharma-continuum/fast-syntax-highlighting.git $HOME/.zsh/fsh
-# git clone https://ghproxy.com/https://github.com/zsh-users/zsh-completions.git $HOME/.zsh/zsh-completions
-
 cargo install starship
 
 cd dotfiles
