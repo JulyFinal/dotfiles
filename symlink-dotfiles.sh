be3e0ee3db0dd44b1ff797162643e4012aeb30c7
julyfinal <julyfinal@icloud.com>
2023-03-30 19:07:55 +0800

    u home

--- symlink-dotfiles.sh
+++ symlink-dotfiles.sh
@@ -20,6 +20,7 @@ link() {
 }
 
 link "$dotfiles/home/zshrc" "$HOME/.zshrc"
+link "$dotfiles/home/bashrc" "$HOME/.bashrc"
 link "$dotfiles/home/ideavimrc" "$HOME/.ideavimrc"
 link "$dotfiles/home/zsh" "$HOME/.zsh"
 # link "$dotfiles/home/tmux.conf" "$HOME/.tmux.conf"
