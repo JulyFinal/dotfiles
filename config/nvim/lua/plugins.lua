137c3a9f0c5a4434c580da7157c17d32f4f63fac
julyfinal <julyfinal@outlook.com>
2023-08-28 14:46:10 +0800

    u

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -148,26 +148,34 @@ local plugins = {
 		"folke/flash.nvim",
 		event = "VeryLazy",
 		---@type Flash.Config
-		opts = {},
+		opts = {
+			modes = {
+				char = { enabled = false },
+			},
+		},
     -- stylua: ignore
     keys = {
-      -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
       { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      -- { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },
-      -- {
-      --   "R",
-      --   mode = { "o", "x" },
-      --   function() require("flash").treesitter_search() end,
-      --   desc =
-      --   "Treesitter Search"
-      -- },
-      -- {
-      --   "<c-s>",
-      --   mode = { "c" },
-      --   function() require("flash").toggle() end,
-      --   desc =
-      --   "Toggle Flash Search"
-      -- },
+      {
+        "gl",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").jump({
+            search = { mode = "search", max_length = 0 },
+            label = { after = { 0, 0 } },
+            pattern = "^"
+          })
+        end,
+        desc = "Jump to a line"
+      },
+      { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },
+      {
+        "R",
+        mode = { "o", "x" },
+        function() require("flash").treesitter_search() end,
+        desc =
+        "Treesitter Search"
+      },
     },
 	},
 
