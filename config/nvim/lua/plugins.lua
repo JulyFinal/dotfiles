be4078064265c8939d318b3e6ee1b10b43256030
julyfinal <julyfinal@outlook.com>
2023-09-22 15:25:19 +0800

    u

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -171,6 +171,27 @@ local plugins = {
 				end,
 				desc = "Jump to a line",
 			},
+			{
+				"gw",
+				mode = { "n", "o", "x" },
+				function()
+					require("flash").jump({
+						pattern = ".", -- initialize pattern with any char
+						search = {
+							mode = function(pattern)
+								-- remove leading dot
+								if pattern:sub(1, 1) == "." then
+									pattern = pattern:sub(2)
+								end
+								-- return word pattern and proper skip pattern
+								return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+							end,
+						},
+						-- select the range
+						jump = { pos = "range" },
+					})
+				end,
+			},
 		},
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
