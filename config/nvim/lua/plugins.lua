c987414c6d56b67bf556e5c7c0021930d2ed29cc
julyfinal <julyfinal@outlook.com>
2023-09-22 15:37:40 +0800

    u

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -176,19 +176,11 @@ local plugins = {
 				mode = { "n", "o", "x" },
 				function()
 					require("flash").jump({
-						pattern = ".", -- initialize pattern with any char
 						search = {
-							mode = function(pattern)
-								-- remove leading dot
-								if pattern:sub(1, 1) == "." then
-									pattern = pattern:sub(2)
-								end
-								-- return word pattern and proper skip pattern
-								return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+							mode = function()
+								return "\\<"
 							end,
 						},
-						-- select the range
-						jump = { pos = "range" },
 					})
 				end,
 			},
