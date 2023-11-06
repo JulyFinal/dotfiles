22ae38aa8878164744033ae360fa98d2476fbd7a
julyfinal <julyfinal@outlook.com>
2023-11-06 10:51:29 +0800

    u

--- config/nvim/lua/plugin-format.lua
+++ config/nvim/lua/plugin-format.lua
@@ -2,7 +2,13 @@ return function()
 	require("conform").setup({
 		formatters_by_ft = {
 			lua = { "stylua" },
-			python = { "black" },
+			python = function(bufnr)
+				if require("conform").get_formatter_info("ruff_format", bufnr).available then
+					return { "ruff_format" }
+				else
+					return { "isort", "black" }
+				end
+			end,
 			-- javascript = { { "prettierd", "prettier" } },
 			bash = { "shfmt" },
 			markdown = { "markdownlint" },
