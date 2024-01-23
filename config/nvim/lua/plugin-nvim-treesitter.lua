b2bb84846b616811d4dbf5d92c52879e724babd8
Final <14924875+JulyFinal@users.noreply.github.com>
2024-01-23 11:57:03 +0800

    Update plugin-nvim-treesitter.lua

--- config/nvim/lua/plugin-nvim-treesitter.lua
+++ config/nvim/lua/plugin-nvim-treesitter.lua
@@ -1,4 +1,7 @@
 return function()
+	for _, config in pairs(require("nvim-treesitter.parsers").get_parser_configs()) do
+  		config.install_info.url = config.install_info.url:gsub("https://github.com/", "https://mirror.ghproxy.com/https://github.com/")
+	end
 	require("nvim-treesitter.configs").setup({
 		ensure_installed = { "lua", "python", "toml", "bash", "html", "css", "markdown", "json", "nix" },
 		sync_install = true,
