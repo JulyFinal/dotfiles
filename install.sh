611aba3a765c0bac9bd343edf9c8032fe9896cda
Final <14924875+JulyFinal@users.noreply.github.com>
2024-03-01 16:12:59 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,26 +4,15 @@
 
 sh <(curl -L https://nixos.org/nix/install) --no-daemon
 
-cargo install starship
-
-# if git is fail
 # git config --global url."https://ghproxy.com/https://github.com".insteadOf "https://github.com"
 git clone https://github.com/JulyFinal/dotfiles.git
 # git clone git@github.com:JulyFinal/dotfiles.git
 
-cargo install starship
-
 cd dotfiles
 sh symlink-dotfiles.sh
 
-
-## nvim
-# install nix format tool
-nix-env -iA nixpkgs-fmt
-
-
 ## fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/FiraMono.zip
-unzip FiraMono.zip -d FiraMono
-mv FiraMono /usr/share/fonts/
-fc-cache -fv
+# wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.1/FiraMono.zip
+# unzip FiraMono.zip -d FiraMono
+# mv FiraMono /usr/share/fonts/
+# fc-cache -fv
