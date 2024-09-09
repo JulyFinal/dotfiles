fbf3a788412f2e6ce37285aee40e83886f090887
final <julyfinal@outlook.com>
2024-09-09 11:22:16 +0800

    u

--- install.sh
+++ install.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
 
-sh <(curl -L https://nixos.org/nix/install) --no-daemon  # install nix
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # install rustup
-# rustup default stable
-
-# bash ble
-nix-env -iA nixpkgs.blesh
-
-# uv
-cargo install --git https://github.com/astral-sh/uv uv # or use brew: brew install uv
-
 # git config --global url."https://ghproxy.com/https://github.com".insteadOf "https://github.com"
 git clone https://github.com/JulyFinal/dotfiles.git
 # git clone git@github.com:JulyFinal/dotfiles.git
 
-cd dotfiles
-sh symlink-dotfiles.sh
+cd dotfiles && sh symlink-dotfiles.sh
 
 ## fonts
 # mkdir -p ~/.local/share/fonts
@@ -25,3 +14,28 @@ sh symlink-dotfiles.sh
 # fc-cache -fv
 ## or use paru
 # paru -S otf-firamono-nerd ttf-jetbrains-mono-nerd
+
+## install base pkg
+sudo pacman -S --needed base-devel
+sudo pacman -S fd ripgrep xsel fzf bat bottom fd lsd sd tealdeer zoxide zip unzip direnv openssl zlib xz tk
+sudo pacman -S neovim lazygit proxychains-ng
+
+
+sh <(curl -L https://nixos.org/nix/install) --no-daemon  # install nix
+
+# bash ble
+nix-env -iA nixpkgs.blesh
+
+# rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh # install rustup
+# sudo pacman -S rustup
+# rustup default stable
+
+# python
+cargo install --git https://github.com/astral-sh/uv uv # or use brew: brew install uv
+
+# install paru
+git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si
+
+# WPS
+# paru -S wps-office wps-office-mui-zh-cn ttf-wps-fonts
