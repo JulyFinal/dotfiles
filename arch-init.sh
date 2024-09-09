285a6a44e5fd939c92246b28db8124e2326d535d
final <julyfinal@outlook.com>
2024-09-09 10:49:07 +0800

    u

--- arch-init.sh
+++ arch-init.sh
@@ -4,7 +4,6 @@
 sudo pacman -S --needed base-devel
 sudo pacman -S fd ripgrep xsel fzf bat bottom fd lsd sd tealdeer zoxide zip unzip direnv openssl zlib xz tk
 sudo pacman -S neovim lazygit proxychains-ng
-sudo pacman -S zip unzip
 
 # install paru
 git clone https://aur.archlinux.org/paru.git && cd paru && makepkg -si
