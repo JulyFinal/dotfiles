ae14cc1cf5a973f6da8731fd5075bed792b9e9d3
final <julyfinal@outlook.com>
2024-10-08 11:13:18 +0800

    u

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -252,6 +252,7 @@ local plugins = {
 		event = "VeryLazy", -- Or `LspAttach`
 		config = function()
 			require("tiny-inline-diagnostic").setup()
+      vim.diagnostic.config({ virtual_text = false })
 		end,
 	},
 }
