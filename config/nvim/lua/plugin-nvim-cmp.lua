42261db7e560e88efce8a52f877198589cff9f07
final <julyfinal@outlook.com>
2024-10-08 15:56:51 +0800

    u

--- config/nvim/lua/plugin-nvim-cmp.lua
+++ config/nvim/lua/plugin-nvim-cmp.lua
@@ -19,7 +19,7 @@ return function()
 		["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 		["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 		["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
-		["<c-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false }),
+		["<C-y>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = true }),
 	}
 
 	cmp.setup({
