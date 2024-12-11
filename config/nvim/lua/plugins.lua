100f5448aceeaebe054dce074140f655b97cfdbb
final <julyfinal@outlook.com>
2024-12-11 15:21:09 +0800

    u

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -35,12 +35,25 @@ local plugins = {
 			require("dashboard").setup({
 				config = {
 					header = {
-						" ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
-						" ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
-						" ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
-						" ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
-						" ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
-						" ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+						"",
+						"",
+						"        ⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀  ",
+						"      ⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷ ",
+						"     ⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿ ",
+						"    ⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿ ",
+						"  ⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿ ",
+						" ⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿ ",
+						" ⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟ ",
+						" ⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣ ",
+						" ⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾ ",
+						" ⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿ ",
+						" ⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿ ",
+						" ⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿ ",
+						" ⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿ ",
+						"  ⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋  ⣾⡌⢠⣿⡿⠃ ",
+						" ⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉            ",
+						"",
+						"",
 					},
 					shortcut = {
 						{ icon = "󰏕  ", desc = "Update", icon_hl = "@property", action = "Lazy update", key = "u" },
@@ -315,7 +328,218 @@ local plugins = {
 	{
 		"nvim-lualine/lualine.nvim",
 		event = "BufEnter",
-		config = require("plugin-lualine"),
+		config = function()
+			local lualine = require("lualine")
+			local colors = {
+				bg = "#202328",
+				fg = "#bbc2cf",
+				yellow = "#ECBE7B",
+				cyan = "#008080",
+				darkblue = "#081633",
+				green = "#98be65",
+				orange = "#FF8800",
+				violet = "#a9a1e1",
+				magenta = "#c678dd",
+				blue = "#51afef",
+				red = "#ec5f67",
+			}
+
+			local conditions = {
+				buffer_not_empty = function()
+					return vim.fn.empty(vim.fn.expand("%:t")) ~= 1
+				end,
+				hide_in_width = function()
+					return vim.fn.winwidth(0) > 80
+				end,
+				check_git_workspace = function()
+					local filepath = vim.fn.expand("%:p:h")
+					local gitdir = vim.fn.finddir(".git", filepath .. ";")
+					return gitdir and #gitdir > 0 and #gitdir < #filepath
+				end,
+			}
+
+			-- Config
+			local config = {
+				options = {
+					component_separators = "",
+					section_separators = "",
+					theme = {
+						normal = { c = { fg = colors.fg, bg = colors.bg } },
+						inactive = { c = { fg = colors.fg, bg = colors.bg } },
+					},
+				},
+				sections = {
+					-- these are to remove the defaults
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {},
+
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				},
+				inactive_sections = {
+					-- these are to remove the defaults
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = {},
+
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = {},
+				},
+				extensions = { "neo-tree" },
+			}
+
+			-- Inserts a component in lualine_c at left section
+			local function ins_left(component)
+				table.insert(config.sections.lualine_c, component)
+			end
+
+			-- Inserts a component in lualine_x ot right section
+			local function ins_right(component)
+				table.insert(config.sections.lualine_x, component)
+			end
+
+			ins_left({
+				function()
+					return "▊"
+				end,
+				color = { fg = colors.blue }, -- Sets highlighting of component
+				padding = { left = 0, right = 1 }, -- We don't need space before this
+			})
+
+			ins_left({
+				-- mode component
+				function()
+					return ""
+				end,
+				color = function()
+					-- auto change color according to neovims mode
+					local mode_color = {
+						n = colors.red,
+						i = colors.green,
+						v = colors.blue,
+						[""] = colors.blue,
+						V = colors.blue,
+						c = colors.magenta,
+						no = colors.red,
+						s = colors.orange,
+						S = colors.orange,
+						ic = colors.yellow,
+						R = colors.violet,
+						Rv = colors.violet,
+						cv = colors.red,
+						ce = colors.red,
+						r = colors.cyan,
+						rm = colors.cyan,
+						["r?"] = colors.cyan,
+						["!"] = colors.red,
+						t = colors.red,
+					}
+					return { fg = mode_color[vim.fn.mode()] }
+				end,
+				padding = { right = 1 },
+			})
+
+			ins_left({
+				-- filesize component
+				"filesize",
+				cond = conditions.buffer_not_empty,
+			})
+
+			ins_left({
+				"filename",
+				cond = conditions.buffer_not_empty,
+				color = { fg = colors.magenta, gui = "bold" },
+			})
+
+			ins_left({ "location" })
+
+			ins_left({ "progress", color = { fg = colors.fg, gui = "bold" } })
+
+			ins_left({
+				"diagnostics",
+				sources = { "nvim_diagnostic" },
+				symbols = { error = " ", warn = " ", info = " " },
+				diagnostics_color = {
+					error = { fg = colors.red },
+					warn = { fg = colors.yellow },
+					info = { fg = colors.cyan },
+				},
+			})
+
+			-- Insert mid section. You can make any number of sections in neovim :)
+			-- for lualine it's any number greater then 2
+			ins_left({
+				function()
+					return "%="
+				end,
+			})
+
+			ins_left({
+				-- Lsp server name .
+				function()
+					local msg = "No Active Lsp"
+					local clients = vim.lsp.get_clients({ bufnr = 0 })
+
+					return #clients > 0
+							and table.concat(
+								vim.tbl_map(function(client)
+									return client.name
+								end, clients),
+								","
+							)
+						or msg
+				end,
+				icon = " LSP:",
+				color = { fg = "#ffffff", gui = "bold" },
+			})
+
+			-- Add components to right sections
+			ins_right({
+				"o:encoding", -- option component same as &encoding in viml
+				fmt = string.upper, -- I'm not sure why it's upper case either ;)
+				cond = conditions.hide_in_width,
+				color = { fg = colors.green, gui = "bold" },
+			})
+
+			ins_right({
+				"fileformat",
+				fmt = string.upper,
+				icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
+				color = { fg = colors.green, gui = "bold" },
+			})
+
+			ins_right({
+				"branch",
+				icon = "",
+				color = { fg = colors.violet, gui = "bold" },
+			})
+
+			ins_right({
+				"diff",
+				-- Is it me or the symbol for modified us really weird
+				symbols = { added = " ", modified = "󰝤 ", removed = " " },
+				diff_color = {
+					added = { fg = colors.green },
+					modified = { fg = colors.orange },
+					removed = { fg = colors.red },
+				},
+				cond = conditions.hide_in_width,
+			})
+
+			ins_right({
+				function()
+					return "▊"
+				end,
+				color = { fg = colors.blue },
+				padding = { left = 1 },
+			})
+
+			-- Now don't forget to initialize lualine
+			lualine.setup(config)
+		end,
 	},
 
 	-- lsp dev
@@ -369,33 +593,22 @@ local plugins = {
 		},
 		config = function(_, opts)
 			local lspconfig = require("lspconfig")
-
-			-- local on_attach = function(client, bufnr)
-			-- 	-- Enable completion triggered by <c-x><c-o>
-			-- 	vim.api.nvim_set_option_value("omnifunc", "v:lua.vim.lsp.omnifunc", { buf = bufnr })
-			-- 	if client.name == "ruff_lsp" then
-			-- 		-- Disable hover in favor of Pyright
-			-- 		client.server_capabilities.hoverProvider = false
-			-- 	end
-			-- end
-			--
-			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			--
-			-- for lsp, settings in pairs(servers) do
-			-- 	lspconfig[lsp].setup({
-			-- 		on_attach = on_attach,
-			-- 		capabilities = capabilities,
-			-- 		settings = settings,
-			-- 	})
-			-- end
-
 			for server, config in pairs(opts.servers) do
-				-- passing config.capabilities to blink.cmp merges with the capabilities in your
-				-- `opts[server].capabilities, if you've defined it
 				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
 				lspconfig[server].setup(config)
 			end
 		end,
+	},
+
+	-- LSP Plugins
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
 	},
 
 	-- complete
@@ -408,7 +621,32 @@ local plugins = {
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
-			keymap = { preset = "default" },
+			keymap = {
+				preset = "default",
+				["<C-q>"] = { "show", "show_documentation", "hide_documentation" },
+				["<C-e>"] = { "hide", "fallback" },
+
+				["<Tab>"] = {
+					function(cmp)
+						if cmp.snippet_active() then
+							return cmp.accept()
+						else
+							return cmp.select_and_accept()
+						end
+					end,
+					"snippet_forward",
+					"fallback",
+				},
+				["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+				["<Up>"] = { "select_prev", "fallback" },
+				["<Down>"] = { "select_next", "fallback" },
+				["<C-p>"] = { "select_prev", "fallback" },
+				["<C-n>"] = { "select_next", "fallback" },
+
+				["<C-b>"] = { "scroll_documentation_up", "fallback" },
+				["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			},
 
 			appearance = {
 				use_nvim_cmp_as_default = true,
@@ -423,6 +661,9 @@ local plugins = {
 			completion = {
 				enabled_providers = { "lsp", "path", "snippets", "buffer", "lazydev", "ripgrep" },
 				accept = { auto_brackets = { enabled = true } },
+				trigger = {
+					show_in_snippet = false,
+				},
 			},
 
 			providers = {
@@ -448,17 +689,6 @@ local plugins = {
 		-- allows extending the providers array elsewhere in your config
 		-- without having to redefine it
 		opts_extend = { "sources.default" },
-	},
-
-	-- LSP Plugins
-	{
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-			},
-		},
 	},
 
 	-- rust
