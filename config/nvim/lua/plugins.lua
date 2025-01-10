9be1f70283c5d4d2793a4b7e7625c478a148dd35
final <julyfinal@outlook.com>
2025-01-10 09:15:37 +0800

    u

--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -388,6 +388,27 @@ local plugins = {
 						},
 					},
 				},
+				["nixd"] = {
+					cmd = { "nixd" },
+					settings = {
+						nixd = {
+							nixpkgs = {
+								expr = "import <nixpkgs> { }",
+							},
+							formatting = {
+								command = { "nixfmt" },
+							},
+							options = {
+								nixos = {
+									expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+								},
+								home_manager = {
+									expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 		config = function(_, opts)
